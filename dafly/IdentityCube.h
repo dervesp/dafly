3b8b342f3a07d75d1de1399cf76acbddd24ddcc0
Vladimir Danilevskiy <dervesp@gmail.com>
2016-10-25 19:30:31 +0300

    initial commit: *базовый проект на основе лабораторной из главы 2 *библиотека libutils *настроенные props для базового проекта и opengl

---
+++ dafly/IdentityCube.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "IBody.h"
+#include "MeshP3NT2.h"
+#include "libutil.h"
+#include <vector>
+#include <array>
+
+enum class CubeFace
+{
+    Front = 0,
+    Back,
+    Top,
+    Bottom,
+    Left,
+    Right,
+
+    NumFaces
+};
+
+class CIdentityCube : public IBody
+{
+public:
+    void Update(float) final;
+    void Draw()const final;
+
+    void SetFaceTextureRect(CubeFace face, const CFloatRect &rect);
+
+private:
+    void Triangulate();
+
+    static const size_t FACES_COUNT = static_cast<size_t>(CubeFace::NumFaces);
+
+    SMeshP3NT2 m_mesh;
+    std::array<CFloatRect, FACES_COUNT> m_textureRects;
+    bool m_isDirty = true;
+};
---
+++ dafly/Window.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "libutil.h"
+#include "IBody.h"
+#include "Lights.h"
+#include "Skybox.h"
+#include "Decorators.h"
+#include <vector>
+
+class CWindow : public CAbstractInputControlWindow
+{
+public:
+    CWindow();
+
+protected:
+    // CAbstractWindow interface
+    void OnWindowInit(const glm::ivec2 &size) override;
+    void OnUpdateWindow(float deltaSeconds) override;
+    void OnDrawWindow(const glm::ivec2 &size) override;
+
+    // IInputEventAcceptor interface
+    void OnKeyDown(const SDL_KeyboardEvent &) override;
+    void OnKeyUp(const SDL_KeyboardEvent &) override;
+
+private:
+    void SetupView(const glm::ivec2 &size);
+
+    CPhongModelMaterial m_material;
+    CTexture2DUniquePtr m_pEarthTexture;
+    CAnimatedDecorator m_decoratedSphere;
+    std::unique_ptr<CSkybox> m_pSkybox;
+    CCamera m_camera;
+    CDirectedLightSource m_sunlight;
+};
